779359b3073d70dad5d4efa77df00e5b7df92ba4
“lcc” <chengcheng.li@51hgp.com>
2024-05-08 16:08:41 +0800

    feat: 笔记编辑优化: formAction

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,13 +3,15 @@ import { addNote, updateNote, delNote } from '@/lib/redis';
 import { redirect } from 'next/navigation';
 
 // 保存笔记
-export async function saveNote(noteId, title, body){
+export async function saveNote(formData){
+  const noteId = formData.get('noteId')
+  const title = formData.get('title')
+  const content = formData.get('body')
   const data = JSON.stringify({
     title,
-    content: body,
+    content,
     updateTime: Date.now()
   })
-
   if(noteId){
     // 编辑
     updateNote(noteId, data)
@@ -22,7 +24,8 @@ export async function saveNote(noteId, title, body){
 }
 
 // 删除笔记
-export async function deleteNote(noteId){
+export async function deleteNote(formData){
+  const noteId = formData.get('noteId')
   delNote(noteId)
   redirect('/')
 }
--- src/components/NoteEditor/NoteEditor.tsx
+++ src/components/NoteEditor/NoteEditor.tsx
@@ -21,21 +21,55 @@ export default function NoteEditor({
 
   return (
     <div className="note-editor">
-      <form className="note-editor-form" autoComplete="off">
+      
+      <form className="note-editor-form" autoComplete='off'>
+        <div className="note-editor-menu" role='menubar'>
+          <input type="hidden" name='noteId' value={noteId}   />
+
+          {/* 保存按钮 */}
+          <button 
+            className="note-editor-done" 
+            type='submit' 
+            role="menuitem" 
+            disabled={pending} 
+            formAction={saveNote}
+          >
+            <img src="/checkmark.svg" width="14px" height="10px" alt="" role="presentation" />
+            Done
+          </button>
+
+          {/* 删除按钮 */}
+          { !isDraft && ( 
+            <button 
+              className="note-editor-delete" 
+              role="menuitem" 
+              disabled={pending} 
+              formAction={deleteNote}
+            >
+              <img src="/cross.svg" width="10px" height="10px" alt="" role="presentation" />
+              Delete
+            </button>)
+          }
+        </div>
+
+        {/* 标题 */}
         <label className="offscreen" htmlFor="note-title-input">
           Enter A Title For Note
         </label>
         <input 
           type="text" 
+          name='title'
           id="note-title-input" 
           value={title} 
           onChange={(e) => setTitle(e.target.value)} 
         />
 
+        {/* 内容 */}
         <label className="offscreen" htmlFor="note-body-input">
           Enter A Body For Note
         </label>
         <textarea 
+          name='body'
           id="note-body-input" 
           value={body} 
           onChange={(e)=>setBody(e.target.value)}
@@ -44,29 +78,6 @@ export default function NoteEditor({
 
 
       <div className="note-editor-preview">
-        <form className="note-editor-menu" role="menubar">
-          <button 
-            className="note-editor-done" 
-            type='submit' 
-            role="menuitem" 
-            disabled={pending} 
-            formAction={() => saveNote(noteId, title, body)}
-          >
-            <img src="/checkmark.svg" width="14px" height="10px" alt="" role="presentation" />
-            Done
-          </button>
-          { !isDraft && ( 
-            <button 
-              className="note-editor-delete" 
-              role="menuitem" 
-              disabled={pending} 
-              formAction={() => deleteNote(noteId)}
-            >
-              <img src="/cross.svg" width="10px" height="10px" alt="" role="presentation" />
-              Delete
-            </button>)
-          }
-        </form>
         <div className="label label--preview" role="status">Preview</div>
         <h1 className='note-title'>{title}</h1>
         <NotePreview>{ body }</NotePreview>
